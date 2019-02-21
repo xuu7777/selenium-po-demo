fb89f56bf7bb8f1e828fee9ba2aa26d12943e57f
xujingbo <983072505@qq.com>
2019-02-21 11:22:50 +0800

    feat(*):change idea

---
+++ src/test/java/com/gago/test/core/BasePage.java
@@ -0,0 +1,111 @@
+package com.gago.test.core;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import com.gago.test.util.LogType;
+import com.gago.test.util.Logger;
+
+public class BasePage {
+
+    private static WebDriver driver;
+    private static String pageTitle;
+    private static String pageUrl;
+
+    public BasePage(WebDriver driver) {
+        BasePage.driver = driver;
+    }
+
+    /**
+     * 清除文本框内容
+     *
+     * @param element
+     */
+    protected void clean(WebElement element) {
+        try {
+            if (element.isDisplayed()) {
+                element.clear();
+                Logger.Output(LogType.LogTypeName.INFO, "Element" + element.toString() + "was cleaned.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Logger.Output(LogType.LogTypeName.ERROR, e.getMessage() + ".");
+        }
+    }
+
+    /**
+     * 点击元素
+     *
+     * @param element
+     */
+    protected void click(WebElement element) {
+        try {
+            if (element.isDisplayed()) {
+                element.click();
+                Logger.Output(LogType.LogTypeName.INFO, "Element" + element.toString() + "was clicked.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Logger.Output(LogType.LogTypeName.ERROR, e.getMessage() + ".");
+        }
+    }
+
+    /**
+     * 获取页面标题
+     *
+     * @return pageTitle
+     */
+    protected String getCurrentPageTitle() {
+        pageTitle = driver.getTitle();
+        Logger.Output(LogType.LogTypeName.INFO, "Current page title is " + pageTitle);
+        return pageTitle;
+    }
+
+    /**
+     * 获取页面的url
+     *
+     * @return pageUrl
+     */
+    protected String getCurrentPageUrl() {
+        pageUrl = driver.getCurrentUrl();
+        Logger.Output(LogType.LogTypeName.INFO, "Current page title is " + pageUrl);
+        return pageUrl;
+    }
+
+    /**
+     * 在文本框输入内容
+     *
+     * @param element
+     * @param text
+     */
+    protected void input(WebElement element, String text) {
+        try {
+            if (element.isDisplayed()) {
+                element.clear();
+                Logger.Output(LogType.LogTypeName.INFO, "Clean the value if any in " + element.toString() + ".");
+                element.sendKeys(text);
+                Logger.Output(LogType.LogTypeName.INFO, "Input value is: " + text + ".");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Logger.Output(LogType.LogTypeName.ERROR, e.getMessage() + ".");
+        }
+    }
+
+    /**
+     * 判断一个元素是否在页面显示
+     *
+     * @param string
+     */
+    protected void verifyElementIsPresent(WebElement element) {
+        try {
+            if (element.isDisplayed()) {
+                Logger.Output(LogType.LogTypeName.INFO, "This Element " + element.toString().trim() + " is present.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Logger.Output(LogType.LogTypeName.ERROR, e.getMessage() + ".");
+        }
+    }
+
+}
---
+++ src/test/java/com/gago/test/util/Logger.java
@@ -0,0 +1,50 @@
+package com.gago.test.util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logger {
+
+    private static String OutputFileName = getDateTimeByFormat(new Date(), "yyyyMMdd_HHmmss");
+    private static OutputStreamWriter outputStreamWriter;
+    private static String logFileName;
+    private static boolean LogFlag = true;
+
+    private static void WriteLog(String logEntry) {
+        // 定义日志文件保存路径和日志文件名称
+        logFileName = ".\\log" + "\\" + OutputFileName + ".log";
+        try {
+            File logFile = new File(logFileName);
+            if (!logFile.exists()) {
+                logFile.createNewFile();
+                // 利用OutputStreamWriter往日志文件写内容，字符编码是unicode
+                outputStreamWriter = new OutputStreamWriter(new FileOutputStream(logFileName), "utf-8");
+            }
+            outputStreamWriter.write(logEntry, 0, logEntry.length());
+            outputStreamWriter.flush();
+        } catch (Exception e) {
+            System.out.println(LogType.LogTypeName.ERROR.toString() + ": Failed to write the file " + logFileName);
+            e.printStackTrace();
+        }
+    }
+
+    // 获取当前系统时间，得到格式化时间字符串
+    private static String getDateTimeByFormat(Date date, String format) {
+        SimpleDateFormat df = new SimpleDateFormat(format);
+        return df.format(date);
+    }
+
+    public static void Output(LogType.LogTypeName logTypeName, String logMessage) {
+        Date date = new Date();
+        String logTime = getDateTimeByFormat(date, "yyyy-MM-dd HH:mm:ss.SSS");
+        String logEntry = logTime + " " + logTypeName.name() + ": " + logMessage + "\r\n";
+        System.out.print(logEntry);
+        // 定义一个开关，为True就输出日志，如果你不想输出，改成False
+        if (LogFlag)
+            WriteLog(logEntry);
+    }
+
+}
